87239e3247287cb7b281c849ee98f98a01999cb1
Francisco Cerdas <chicus12@gmail.com>
2021-01-25 15:25:27 -0600

    se cambia algunos textos

--- src/SMS-receiver.js
+++ src/SMS-receiver.js
@@ -6,13 +6,12 @@ export default function SMSReceiver() {
       </h1>
       <div>
         Enviar un SMS que incluya lo siguiente:
-        <pre><code>@web-otp.glitch.me #12345</code></pre>
-        {' '}
-        to this phone.
+        <pre><code>@pwa-apis.vercel.app #12345</code></pre>
+        a este teléfono.
       </div>
       <div style={{ border: '1px solid', padding: '5px 10px', margin: '10px 0' }}>
         <form action="/post" method="post">
-          Enter OTP here:
+          Ingresar el OTP aqui:
           <input type="text" autoComplete="one-time-code" inputMode="numeric" name="one-time-code" />
           <input type="submit" value="Submit" />
         </form>
